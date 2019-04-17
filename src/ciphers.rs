930d4837f7b787258f2ab5c06e57500bd6088e84
Adam Gutglick <adamgsal@gmail.com>
2019-04-17 19:50:26 +0300

    Created breakers modules out of prior solutions

--- src/ciphers.rs
+++ src/ciphers.rs
@@ -14,3 +14,34 @@ pub fn repeating_key_xor_cipher(ciphertext: &[u8], key: &[u8]) -> Vec<u8> {
 
     r
 }
+
+pub mod breakers {
+    use super::single_byte_xor_cipher;
+    use crate::utils::all_chars;
+    use crate::utils::rate_string;
+    use std::collections::HashMap;
+    use std::iter::FromIterator;
+
+    pub fn break_single_xor_cipher(ciphertext: &[u8]) -> u8 {
+        let hex_keys: Vec<u8> = all_chars().iter().map(|c| *c as u8).collect();
+
+        let mut base = 0;
+        let mut suspected_key = 0_u8;
+        for key in hex_keys {
+            let cleartext: Vec<char> = single_byte_xor_cipher(&ciphertext, key)
+                .iter()
+                .map(|b| *b as char)
+                .collect();
+            let cleartext_string = String::from_iter(cleartext);;
+
+            let string_rating = rate_string(&cleartext_string.as_str());
+
+            if string_rating > base {
+                suspected_key = key;
+                base = string_rating
+            }
+        }
+
+        suspected_key
+    }
+}
